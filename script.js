f908e25195c974494f7ce35b835bdb424fe70c90
Elias Kelta <eliaskelta1@gmail.com>
2023-05-23 17:09:05 +0200

    Alles wunderschön kommentiert

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
 function addTodo() {
+    // Input speichern
     let input = document.getElementById("input").value;
     console.log(input);
 
+    // p Element erstellen
     let element = document.createElement("p");
+    // Text Node für das Element erstellen
     let text = document.createTextNode(input);
+    // Text Node an das p Element anhängen
     element.appendChild(text);
+
+    // Div area bekommen
     let area = document.getElementById("area");
+    // p Element an area anhängen
     area.appendChild(element);
+
+    // item Klasse hinzufügen
     element.classList.add("item");
 
-    // Event listener
+    // Event listener hinzufügen
     element.addEventListener("click", () => {
         element.remove();
     });
 }
 
+// Event Listener für den clear button
 document.getElementById("clear_btn").addEventListener("click", () => {
     console.log("clicked");
     clearTodos();
